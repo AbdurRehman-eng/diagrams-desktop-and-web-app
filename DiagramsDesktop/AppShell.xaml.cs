9c078b3893525f4d798a6d000fcba3bf78274bf4
AbdurRehman-eng <shafiqueabdurrehman1@gmail.com>
2026-07-10 21:54:04 +0500

    refactor: remove obsolete solution configuration, upgrade logs, and database schema files

--- DiagramsDesktop/AppShell.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DiagramsDesktop;
-
-public partial class AppShell : Shell
-{
-	public AppShell()
-	{
-		InitializeComponent();
-	}
-}
--- DiagramsDesktop/Platforms/MacCatalyst/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace DiagramsDesktop;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
